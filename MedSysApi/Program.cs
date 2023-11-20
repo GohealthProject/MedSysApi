ad1bc2df7cb72cc989615f14a555d52aee9d4661
順 陳 <night90810@gmail.com>
2023-11-20 15:32:27 +0800

    245

--- MedSysApi/Program.cs
+++ MedSysApi/Program.cs
@@ -1,10 +1,14 @@
 using MedSysApi.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
