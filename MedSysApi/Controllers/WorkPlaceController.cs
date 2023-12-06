34cad2d3f75a997451679f56320b06830a92ea07
DESKTOP-67432ML\s4347 <s434780@hotmail.com>
2023-12-06 11:06:36 +0800

    1206

--- MedSysApi/Controllers/WorkPlaceController.cs
+++ MedSysApi/Controllers/WorkPlaceController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MedSysApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MedSysApi.Controllers
 {
     public class WorkPlaceController : Controller
     {
-        public IActionResult Index()
+        private readonly MedSysContext _context;
+        public IActionResult Index(MedSysContext  context)
         {
+            _context=context;
             return View();
         }
     }
---
+++ MedSysApi/Controllers/planComeparisonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedSysApi.Controllers
+{
+    public class planComeparisonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
