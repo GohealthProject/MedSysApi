47c85a3bd8331e53193fdd349106e857facf4016
Jacky Jie <jakcy2222@gmail.com>
2023-12-10 00:36:37 +0800

    ???

---
+++ MedSysApi/Controllers/PlansController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MedSysApi.Models;
+
+namespace MedSysApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlansController : ControllerBase
+    {
+        private readonly MedSysContext _context;
+
+        public PlansController(MedSysContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Plans
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Plan>>> GetPlans()
+        {
+          if (_context.Plans == null)
+          {
+              return NotFound();
+          }
+            return await _context.Plans.ToListAsync();
+        }
+
+        // GET: api/Plans/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Plan>> GetPlan(int id)
+        {
+          if (_context.Plans == null)
+          {
+              return NotFound();
+          }
+            var plan = await _context.Plans.FindAsync(id);
+
+            if (plan == null)
+            {
+                return NotFound();
+            }
+
+            return plan;
+        }
+
+        [HttpGet("test/{id}")]
+        public async Task<ActionResult<Plan>> GPlan(int id)
+        {
+            var q = from p in _context.Plans
+                select p;
+
+
+            return Ok(q);
+
+        }
+
+
+        // PUT: api/Plans/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlan(int id, Plan plan)
+        {
+            if (id != plan.PlanId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(plan).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlanExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Plans
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Plan>> PostPlan(Plan plan)
+        {
+          if (_context.Plans == null)
+          {
+              return Problem("Entity set 'MedSysContext.Plans'  is null.");
+          }
+            _context.Plans.Add(plan);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPlan", new { id = plan.PlanId }, plan);
+        }
+
+        // DELETE: api/Plans/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlan(int id)
+        {
+            if (_context.Plans == null)
+            {
+                return NotFound();
+            }
+            var plan = await _context.Plans.FindAsync(id);
+            if (plan == null)
+            {
+                return NotFound();
+            }
+
+            _context.Plans.Remove(plan);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PlanExists(int id)
+        {
+            return (_context.Plans?.Any(e => e.PlanId == id)).GetValueOrDefault();
+        }
+    }
+}
