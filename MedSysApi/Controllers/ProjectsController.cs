2fb00458df3cbc92c9a6bebb48408a48b75bd0ee
Jacky Jie <jakcy2222@gmail.com>
2023-12-19 16:06:54 +0800

    拜託沒事

---
+++ MedSysApi/Controllers/ProjectsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MedSysApi.Models;
+
+namespace MedSysApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly MedSysContext _context;
+
+        public ProjectsController(MedSysContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Projects
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
+        {
+          if (_context.Projects == null)
+          {
+              return NotFound();
+          }
+            return await _context.Projects.ToListAsync();
+        }
+
+        // GET: api/Projects/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> GetProject(int id)
+        {
+          if (_context.Projects == null)
+          {
+              return NotFound();
+          }
+            var project = await _context.Projects.FindAsync(id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return project;
+        }
+
+        // PUT: api/Projects/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProject(int id, Project project)
+        {
+            if (id != project.ProjectId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(project).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Projects
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Project>> PostProject(Project project)
+        {
+          if (_context.Projects == null)
+          {
+              return Problem("Entity set 'MedSysContext.Projects'  is null.");
+          }
+            _context.Projects.Add(project);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProjectExists(project.ProjectId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetProject", new { id = project.ProjectId }, project);
+        }
+
+        [HttpPost]
+        public IActionResult addProject()
+        {
+            var q = Request.Form;
+
+            string pjname = q["Cprjchk"];
+
+
+
+            return Ok();
+        }
+
+        // DELETE: api/Projects/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            if (_context.Projects == null)
+            {
+                return NotFound();
+            }
+            var project = await _context.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return (_context.Projects?.Any(e => e.ProjectId == id)).GetValueOrDefault();
+        }
+    }
+}
