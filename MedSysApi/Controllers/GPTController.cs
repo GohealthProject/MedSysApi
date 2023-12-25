160626e1d2fbf1892db5bccb57f27e202c3c1160
Jacky Jie <jakcy2222@gmail.com>
2023-12-25 17:35:45 +0800

    GPT測試

---
+++ MedSysApi/Controllers/GPTController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace MedSysApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GPTController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> GetAIBasedResult(string SearchText)
+        {
+            string APIKey = "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";
+            string answer = string.Empty;
+
+            var openai = new OpenAIAPI(APIKey);
+            CompletionRequest req = new CompletionRequest()
+            {
+                Prompt = SearchText,
+                Model = OpenAI_API.Models.Model.DavinciText,
+                MaxTokens = 200
+            };
+
+            var result = await openai.Completions.CreateCompletionAsync(req);
+
+            foreach (var choice in result.Completions)
+            {
+                answer = choice.Text;
+            }
+
+            return Ok(answer);
+        }
+
+    }
+}
